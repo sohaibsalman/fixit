7f989c5d8479e1976eb5841274a701a03aa3af94
Sohaib Salman <53570067+sohaibsalman@users.noreply.github.com>
2019-12-28 16:11:52 +0500

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
+//Animation on scroll
 $(function () {
     new WOW().init();
+});
+
+//Scroll Ease
+$(function () {
+    $("a.smooth-scroll").click(function (event) {
+        event.preventDefault();
+
+        var sectionID = $(this).attr('href');
+
+        $('html, body').animate(
+            {
+                scrollTop: $(sectionID).offset().top - 64
+            }, 1250, "easeInOutExpo"
+        );
+    })
 });
